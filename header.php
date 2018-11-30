c3919b41b828dfbb7c12b6d28826b1017aa40c51
user <user@usernoMacBook-puro.local>
2018-11-30 11:05:21 +0900

    <head>修正

--- header.php
+++ header.php
@@ -1,34 +1,15 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/website#">
-<?php
-$folderUrl = $_SERVER["REQUEST_URI"] ;
-$tempUrl = '//'. $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
-$para = $_SERVER['QUERY_STRING'];
-$str=str_replace('?'.$para,"",$tempUrl);
-$url= rtrim($str, '/');
-?>
+
   <title><?php the_title(); ?></title>
 
   <meta charset="UTF-8">
   <meta name="theme-color" content="#008cb1">
   <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
-  <meta name="description" content="<?php bloginfo('description'); ?>">
-
-  <meta property="og:site_name" content="<?php bloginfo('name'); ?>">
-  <meta property="og:title" content="<?php the_title(); ?>">
-  <meta property="og:type" content="website">
-  <meta property="og:url" content="<?php echo $url; ?>">
-  <meta property="og:description" content="<?php bloginfo('description'); ?>">
-  <meta property="og:image" content="" />
-
-  <meta property="fb:app_id" content="286943355041198" />
-
-  <meta name="twitter:card" content="photo">
-  <meta name="twitter:site" content="">
 
   <!-- <link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png"> -->
-  <link rel="canonical" href="<?php echo $url; ?>">
+
   <link rel="dns-prefetch" href="//maxcdn.bootstrapcdn.com">
   <link rel="dns-prefetch" href="//code.jquery.com">
   <link rel="dns-prefetch" href="//pagead2.googlesyndication.com">
@@ -44,17 +25,20 @@ $url= rtrim($str, '/');
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
   <script src="<?php echo get_template_directory_uri(); ?>/js/picturefill.min.js"></script>
+
   <?php wp_head(); ?>
-  <div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v3.2';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v3.2';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+
 </head>
 <body class="<?php if(is_home()){echo 'home ';} ?>">
+<div id="fb-root"></div>
 <div class="spwrap" id="spWrap">
 <div class="ly_fixed">
   <header class="ly_header">
